2930c6f87916c990b147979a92b4ac4ec923f3db
Krona-org <krona.original@gmail.com>
2026-07-11 01:47:03 +0300

    Добавил стиль кнопок слайд бара

--- src/stypehelper.hpp
+++ src/stypehelper.hpp
@@ -6,6 +6,7 @@ class StyleHelper
 public:
     static inline QString getMainWidgetStyle(const QString &imagePath);
     static inline QString getGlassStyle();
+    static inline QString getStyleBtnSlideBar();
 };
 
 
@@ -34,4 +35,32 @@ QString StyleHelper::getGlassStyle(){
         }
     )";
 
+}
+
+QString StyleHelper::getStyleBtnSlideBar(){
+    return R"(
+        QPushButton {
+            background-color: rgb(17, 23, 24);
+            color: rgb(199, 212, 210);
+            border: 1px solid rgb(33, 48, 50);
+            border-radius: 6px;
+            text-align: left;
+            padding-left: 16px;
+            font-family: Consolas;
+            font-size: 10pt;
+        }
+        QPushButton:hover {
+            background-color: rgb(22, 30, 32);
+            border-color: rgb(51, 64, 63);
+        }
+        QPushButton:pressed {
+            background-color: rgb(255, 140, 0);
+            color: rgb(12, 16, 17);
+        }
+        QPushButton:checked {
+            background-color: rgb(255, 140, 0);
+            color: rgb(12, 16, 17);
+            border-color: rgb(255, 178, 56);
+        }
+        )";
 }

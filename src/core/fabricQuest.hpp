d4683d23e738d074ea4ec7b7e60ff0e07f86c0fb
Krona-org <danil.marushkin@yandex.ru>
2026-07-27 02:06:09 +0300

    Добавил метод обращения к родительскому классу Question и перенес реализоцию методов всне класса

--- src/core/fabricQuest.hpp
+++ src/core/fabricQuest.hpp
@@ -8,43 +8,55 @@
 
 // переписываем заново 
 
-class FabricQuest
+class FabricQuest final
 {
 public:
-    FabricQuest() 
-    {
-        registerType<oneVarQuestion>(0);
-        registerType<twoVarQuestion>(1);
-        registerType<threeVarQuestion>(2);
-    };
-    void push_back(int indexPage, QString BodyTittle, QString BrowCode, QString BodyCode,
-                     QString BrowSolution, QString BodySolution, QVector<AnswerOption> Answer)
-    {
-        auto it = creators.find(indexPage);
-        if(it == creators.end())
-            throw std::invalid_argument("uncnown indexPage");
+    FabricQuest();
+    void push_back(int indexPage, QString BodyTitle,
+                    QString BrowCode, QString BodyCode,
+                    QString BrowSolution, QString BodySolution,
+                    QVector<AnswerOption> Answer);
+    std::shared_ptr<Question> getQuestion(int indexPage, int num) const;
 
-        mapQuest[indexPage].push_back(it->second(BodyTittle, BrowCode, BodyCode,
-                                                    BrowSolution, BodySolution, Answer));
-    }
-    int getIndex(int indexPage, int num) 
-    { 
-        return mapQuest.at(indexPage).at(num)->getPrivateId();
-    }
-    QString getBodySolution(int indexPage, int num) const
-    {
-        return mapQuest.at(indexPage).at(num)->getBodySolution();
-    }
 private:
     template<typename T>
-    void registerType(int indexPage)
-    {
-        creators[indexPage] = [](QString BodyTitle, QString BrowCode, QString BodyCode,
-                                  QString BrowSolution, QString BodySolution, QVector<AnswerOption> Answer) 
-        {
-            return std::make_shared<T>(BodyTitle, BrowCode, BodyCode, BrowSolution, BodySolution, Answer);
-        };
-    }
-    std::map<int, std::function<std::shared_ptr<Question>(QString, QString, QString, QString, QString, QVector<AnswerOption>)>> creators;
+    void registerType(int indexPage);
+
     std::map<int, std::vector<std::shared_ptr<Question>>> mapQuest;
+    std::map<int, std::function<std::shared_ptr<Question>(QString, QString,
+                                                            QString, QString,
+                                                            QString, QVector<AnswerOption>)>> creators;
 };
+
+template<typename T>
+inline void FabricQuest::registerType(int indexPage)
+{
+    creators[indexPage] = [](QString BodyTitle, QString BrowCode, QString BodyCode,
+                                  QString BrowSolution, QString BodySolution, QVector<AnswerOption> Answer) 
+    {
+        return std::make_shared<T>(BodyTitle, BrowCode, BodyCode, BrowSolution, BodySolution, Answer);
+    };
+}
+inline FabricQuest::FabricQuest()
+{
+    registerType<oneVarQuestion>(0);
+    registerType<twoVarQuestion>(1);
+    registerType<threeVarQuestion>(2);
+};
+
+inline void FabricQuest::push_back(int indexPage, QString BodyTitle,
+                                    QString BrowCode, QString BodyCode,
+                                    QString BrowSolution, QString BodySolution,
+                                    QVector<AnswerOption> Answer)
+{
+    auto it = creators.find(indexPage);
+    if(it == creators.end())
+        throw std::invalid_argument("uncnown indexPage");
+
+    mapQuest[indexPage].push_back(it->second(BodyTitle, BrowCode, BodyCode,
+                                                BrowSolution, BodySolution, Answer));
+}
+inline std::shared_ptr<Question> FabricQuest::getQuestion(int indexPage, int num) const
+{
+    return mapQuest.at(indexPage).at(num);
+}

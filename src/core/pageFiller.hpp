2aa8821cd54e8b54cc8f192432bc8680f244306d
Krona-org <danil.marushkin@yandex.ru>
2026-08-01 00:44:26 +0300

    Исправил передачу параметров в метод

--- src/core/pageFiller.hpp
+++ src/core/pageFiller.hpp
@@ -10,19 +10,31 @@ class PageFiller
 {
 public:
     PageFiller(Ui::MainWindow* ui) : ui(ui) {};
-    void fillPage_1(std::shared_ptr<Question> oneQuest);
-    void fillPage_2(twoVarQuestion* twoQuestion);
-    void fillPage_3(threeVarQuestion* threeQuestion);
+    void fillPage_1(std::shared_ptr<Question> Quest);
+    void fillPage_2(std::shared_ptr<Question> Quest);
+    void fillPage_3(std::shared_ptr<Question> Quest);
 private:
     Ui::MainWindow *ui;
 };
-inline void PageFiller::fillPage_1(std::shared_ptr<Question> oneQuest)
+inline void PageFiller::fillPage_1(std::shared_ptr<Question> Quest)
 {
-    ui->bodyTitle_page1->setText(oneQuest->getBodyTitle());
-    ui->browCode_page1->setText(oneQuest->getBrowCode());
-    ui->bodyCode_page1->setPlainText(oneQuest->getBodyCode());
-    ui->browSolution_page1->setText(oneQuest->getBrowSolution());
-    ui->bodySolution_page1->setText(oneQuest->getBodySolution());
+    ui->bodyTitle_page1->setText(Quest->getBodyTitle());
+    ui->browCode_page1->setText(Quest->getBrowCode());
+    ui->bodyCode_page1->setPlainText(Quest->getBodyCode());
+    ui->browSolution_page1->setText(Quest->getBrowSolution());
+    ui->bodySolution_page1->setText(Quest->getBodySolution());
+    // ui->answerButton1_page1
+    // ui->answerButton2_page1
+    // ui->answerButton3_page1
+    // ui->answerButton4_page1
+}
+inline void PageFiller::fillPage_2(std::shared_ptr<Question> Quest)
+{
+    ui->bodyTitle_page2->setText(Quest->getBodyTitle());
+    ui->browCode_page2->setText(Quest->getBrowCode());
+    ui->bodyCode_page2->setPlainText(Quest->getBodyCode());
+    ui->browSolution_page2->setText(Quest->getBrowSolution());
+    ui->bodySolution_page2->setText(Quest->getBodySolution());
     // ui->answerButton1_page1
     // ui->answerButton2_page1
     // ui->answerButton3_page1

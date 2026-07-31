4dc93ef6ed4547e2f86245b8462ea545001b0a15
Krona-org <danil.marushkin@yandex.ru>
2026-08-01 01:27:48 +0300

    Добавил подсветку синтаксиса и автоматический размер bodyCode

--- src/core/pageFiller.hpp
+++ src/core/pageFiller.hpp
@@ -3,18 +3,24 @@
 
 #include "question.hpp"
 #include "ui_mainwindow.h"
+#include "cppsyntaxhighlighter.hpp"
+#include "codesize.hpp"
 #include <memory>
 
 
 class PageFiller
 {
 public:
-    PageFiller(Ui::MainWindow* ui) : ui(ui) {};
+    PageFiller(Ui::MainWindow* ui) : ui(ui) {
+        highlighter_page1 = new CppSyntaxHighlighter(ui->bodyCode_page1->document());
+    };
     void fillPage_1(std::shared_ptr<Question> Quest);
     void fillPage_2(std::shared_ptr<Question> Quest);
     void fillPage_3(std::shared_ptr<Question> Quest);
 private:
     Ui::MainWindow *ui;
+    CppSyntaxHighlighter *highlighter_page1 = nullptr;
+
 };
 inline void PageFiller::fillPage_1(std::shared_ptr<Question> Quest)
 {
@@ -27,14 +33,17 @@ inline void PageFiller::fillPage_1(std::shared_ptr<Question> Quest)
     // ui->answerButton2_page1
     // ui->answerButton3_page1
     // ui->answerButton4_page1
+    CodeSize::fitCodeEditHeight(ui->bodyCode_page1);   // <-- добавить
+
 }
 inline void PageFiller::fillPage_2(std::shared_ptr<Question> Quest)
 {
-    ui->bodyTitle_page2->setText(Quest->getBodyTitle());
-    ui->browCode_page2->setText(Quest->getBrowCode());
-    ui->bodyCode_page2->setPlainText(Quest->getBodyCode());
-    ui->browSolution_page2->setText(Quest->getBrowSolution());
-    ui->bodySolution_page2->setText(Quest->getBodySolution());
+    // ui->bodyTitle_page2->setText(Quest->getBodyTitle());
+    // ui->browCode_page2->setText(Quest->getBrowCode());
+    // ui->bodyCode_page2->setPlainText(Quest->getBodyCode());
+    // ui->browSolution_page2->setText(Quest->getBrowSolution());
+    // ui->bodySolution_page2->setText(Quest->getBodySolution());
+    // ui->
     // ui->answerButton1_page1
     // ui->answerButton2_page1
     // ui->answerButton3_page1
